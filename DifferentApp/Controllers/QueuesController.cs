45aefefa152bb7a136907cfe8b3f79a6d849544c
Geetika Sethi <geetikas@midfinsystems.com>
2018-02-13 22:54:49 +0530

    Add project files.

---
+++ DifferentApp/Controllers/QueuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace DifferentApp.Controllers
+{
+    public class QueuesController : Controller
+    {
+        // GET: Queues
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddMessage()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+          CloudConfigurationManager.GetSetting("myresourcegroup1diag309_AzureStorageConnectionString"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("tempreading32");
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(10);
+            var timer = new System.Threading.Timer((e) =>
+            {
+                Random rnd = new Random();
+                float temp = rnd.Next(40, 100);
+                CloudQueueMessage message = new CloudQueueMessage(Convert.ToString(temp));
+                queue.AddMessage(message);
+                ViewBag.QueueName = queue.Name;
+                ViewBag.Message = message.AsString;
+
+            }, null, startTimeSpan, periodTimeSpan);
+
+            return View();
+        }
+    }
+}
---
+++ DifferentApp/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DifferentApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+             CloudConfigurationManager.GetSetting("myresourcegroup1diag309_AzureStorageConnectionString"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("tempreading32");
+            queue.CreateIfNotExists();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(AddMessage);
+            aTimer.Interval = 10000;
+            aTimer.Enabled = true;
+
+
+
+
+        }
+
+        public void AddMessage(object source, ElapsedEventArgs e)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+          CloudConfigurationManager.GetSetting("myresourcegroup1diag309_AzureStorageConnectionString"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("tempreading32");
+            Random rnd = new Random();
+            float temp = rnd.Next(40, 100);
+            CloudQueueMessage message = new CloudQueueMessage(Convert.ToString(temp));
+            queue.AddMessage(message);
+
+        }
+
+    }
+}
